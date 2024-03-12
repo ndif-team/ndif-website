dd482c18849a4021a85a9758047929e1123a9910
David Bau <david.bau@gmail.com>
2024-03-12 10:11:05 -0400

    Templatize.

--- src/build.py
+++ src/build.py
@@ -4,20 +4,17 @@ import os
 import shutil
 import importlib.util
 import sys
+import argparse
 
-# Function to load Python modules from the current directory into a single namespace
+# Function to load Python modules from the current directory into the script's namespace
 def load_modules():
     namespace = {}
     script_name = os.path.basename(sys.argv[0])
     for file in os.listdir("."):
         if file.endswith(".py") and file != script_name:
-            module_name = file[:-3]
-            spec = importlib.util.spec_from_file_location(module_name, file)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            for name, obj in vars(module).items():
-                if callable(obj):
-                    namespace[name] = obj
+            with open(file, 'r') as f:
+                code = compile(f.read(), file, 'exec')
+                exec(code, namespace)
     return namespace
 
 # Function to escape triple quotes in the template
@@ -25,11 +22,15 @@ def escape_triple_quotes(template):
     return template.replace('"""', '\\"\\"\\"')
 
 # Function to expand HTML templates using the loaded namespace
-def expand_template(template, namespace):
+def expand_template(template, namespace, pathname):
+    namespace['pathname'] = pathname
     escaped_template = escape_triple_quotes(template)
     try:
         return eval(f'f"""{escaped_template}"""', namespace)
-    except (SyntaxError, NameError, TypeError, ZeroDivisionError):
+    except (SyntaxError):
+        return template
+    except (NameError, TypeError, ZeroDivisionError) as e:
+        print(f'Ignoring error in {pathname}:', e)
         return template
 
 # Function to generate the static website recursively
@@ -46,7 +47,8 @@ def generate_website(src_dir, dest_dir, namespace):
             if item.endswith(".html"):
                 with open(src_path, "r") as f:
                     template = f.read()
-                expanded_html = expand_template(template, namespace)
+                pathname = os.path.join('/', os.path.relpath(src_path, src_dir))
+                expanded_html = expand_template(template, namespace, pathname)
                 with open(dest_path, "w") as f:
                     f.write(expanded_html)
             elif not item.endswith(".py"):
@@ -56,11 +58,17 @@ def generate_website(src_dir, dest_dir, namespace):
 
 # Run the website generation
 def main():
+    # Parse command-line arguments using argparse
+    parser = argparse.ArgumentParser(description='Static website generator')
+    parser.add_argument('-s', '--source', default='./', help='Source directory')
+    parser.add_argument('-t', '--target', default='../public', help='Target directory')
+    args = parser.parse_args()
+
     # Load Python modules into a single namespace
     namespace = load_modules()
 
     # Generate the static website recursively
-    generate_website(".", "../public", namespace)
+    generate_website(args.source, args.target, namespace)
 
     print("Static website generated successfully!")
 
---
+++ src/header.py
@@ -0,0 +1,79 @@
+
+def header():
+    global pathname
+    def nav_link(text, link):
+        classes = ' border rounded-pill' if text == 'Get Involved' else ''
+        attributes = ''
+        if link == pathname:
+            classes += ' active'
+            attributes = ' aria-current="page"'
+        return f'<a class="nav-link text-secondary{classes}"{attributes} href="{link}">{text}</a>'
+
+    return f"""\
+<!doctype html>
+<html lang="en" data-bs-theme="light">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<title>National Deep Inference Fabric</title>
+
+<!-- Bootstrap CSS Imports -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<!-- Google Font Imports -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+rel="stylesheet">
+
+<!-- Custom CSS Imports -->
+<link rel="stylesheet" href="./css/main.css">
+<link rel="stylesheet" href="./css/home.css">
+<link rel="stylesheet" href="./css/custom.css">
+
+</head>
+
+<body>
+
+<header class="pb-3 pt-4">
+<div class="container col-xxl-8">
+<div class="row align-items-center">
+<div class="col-md-4 text-start">
+<a href="./" style="color: inherit; text-decoration: none;">
+<div class="row align-items-center">
+<div class="col-md-4 border-end px-0">
+<img src="./images/NDIF_Acr_color.png" class="d-block img-fluid" width="100"
+height="100" loading="lazy">
+</div>
+<div class="col-md-8 my-auto">
+<h4 style="margin-bottom: 0 !important; color: #656565; margin-bottom: 0 !important; font-size: 22px; line-height: 0.9; font-weight: 700 !important;" class="fw-semibold">
+National Deep<br />Inference Fabric
+</h4>
+</div>
+</div>
+</a>
+</div>
+<div class="col-md-8 align-items-center justify-content-end d-flex">
+<ul class="nav navbar-light p-1" style="display: inline-flex;">
+<li class="nav-item">
+{nav_link('Home', './')}
+</li>
+<li class="nav-item">
+{nav_link('Fabric', './fabric.html')}
+</li>
+<li class="nav-item me-2">
+{nav_link('About Us', './about.html')}
+<a class="nav-link text-secondary active" aria-current="page" href="./about.html">About Us</a>
+</li>
+<li class="nav-item">
+{nav_link('Get Involved', './start.html')}
+</li>
+</ul>
+</div>
+</div>
+</div>
+</header>
+"""
