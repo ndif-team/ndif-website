8c8ace74cc361fb303ba6e799fa22b1a66133b9f
Caden <35741667+cadentj@users.noreply.github.com>
2024-03-13 12:47:46 +0000

    mobile styles and color fixes

--- src/footer.py
+++ src/footer.py
@@ -2,33 +2,37 @@ def footer():
     return f"""
 
 <div class="bg-body-secondary">
-<footer class="container py-5 bg-body-secondary">
-<div class="row">
-<div class="col-12 col-md">
-<h5>NDIF</h5>
-<small class="d-block mb-3 text-body-secondary">© 2024 NDIF</small>
-</div>
-<div class="col-6 col-md">
-<ul class="list-unstyled text-small">
-<li><a class="link-secondary text-decoration-none" href="/">Home</a></li>
-<li><a class="link-secondary text-decoration-none" href="/fabric.html">Fabric</a></li>
-<li><a class="link-secondary text-decoration-none" href="/about.html">About Us</a></li>
-</ul>
-</div>
-<div class="col-6 col-md">
-<ul class="list-unstyled text-small">
-<li><a class="link-secondary text-decoration-none" href="/start.html">Get involved</a></li>
-<li><a class="link-secondary text-decoration-none" href="https://nnsight.net/">NNsight</a></li>
-<li><a class="link-secondary text-decoration-none" href="/signup.html">Community</a></li>
-</ul>
-</div>
-</div>
-</footer>
+    <footer class="container py-5 bg-body-secondary">
+        <div class="row">
+            <div class="col-12 col-md">
+                <h5>NDIF</h5>
+                <small class="d-block mb-3 text-body-secondary">© 2024 NDIF</small>
+            </div>
+            <div class="col-6 col-md">
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary text-decoration-none" href="/">Home</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="/fabric.html">Fabric</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="/about.html">About Us</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary text-decoration-none" href="/start.html">Get involved</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="https://nnsight.net/">NNsight</a></li>
+                    <li><a class="link-secondary text-decoration-none" href="/signup.html">Community</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <img src="./images/northeastern.svg" alt="Description of the image" style="max-width: 5vw;">
+                <p>NDIF is developed at Northeastern University</p>
+            </div>
+        </div>
+    </footer>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-crossorigin="anonymous"></script>
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 
 </body>
 
